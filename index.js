a39ebde71eae153defd514bbde9923877e08a72b
neilalden <escobin.neil@gmail.com>
2022-04-19 02:45:35 +0800

    replaced shp display of map to svg. added desgins, dummy data generator, and charts

--- index.js
+++ index.js
@@ -1,171 +1,159 @@
-// RENDER MAP
-function renderMap() {
-	var shpURL = "assets/tadlak_lake.shp";
-	var dbfURL = "assets/tadlak_lake.dbf";
-	var shpFile;
-	var dbfFile;
-	var shpLoader = new BinaryAjax(shpURL, onShpComplete, onShpFail);
-	var dbfLoader = new BinaryAjax(dbfURL, onDbfComplete, onDbfFail);
-	function onShpFail() {
-		alert("failed to load " + shpURL);
-	}
-	function onDbfFail() {
-		alert("failed to load " + dbfURL);
-	}
-	function onShpComplete(oHTTP) {
-		var binFile = oHTTP.binaryResponse;
-		if (window.console && window.console.log) shpFile = new ShpFile(binFile);
-		if (
-			shpFile.header.shapeType != ShpType.SHAPE_POLYGON &&
-			shpFile.header.shapeType != ShpType.SHAPE_POLYLINE
-		) {
-			alert(
-				"Shapefile does not contain Polygon records (found type: " +
-					shpFile.header.shapeType +
-					")",
-			);
-		}
-		if (dbfFile) {
-			render(shpFile.records, dbfFile.records);
-		}
-	}
-	function onDbfComplete(oHTTP) {
-		var binFile = oHTTP.binaryResponse;
-		if (window.console && window.console.log) dbfFile = new DbfFile(binFile);
-		if (shpFile) {
-			render(shpFile.records, dbfFile.records);
-		}
-	}
-
-	function render(records, data) {
-		if (window.console && window.console.log)
-			var canvas = document.getElementById("map");
-		if (window.G_vmlCanvasManager) {
-			G_vmlCanvasManager.initElement(canvas);
-		}
-		var t1 = new Date().getTime();
-		if (window.console && window.console.log) var box;
-		for (var i = 0; i < records.length; i++) {
-			var record = records[i];
-			if (
-				record.shapeType == ShpType.SHAPE_POLYGON ||
-				record.shapeType == ShpType.SHAPE_POLYLINE
-			) {
-				var shp = record.shape;
-				for (var j = 0; j < shp.rings.length; j++) {
-					var ring = shp.rings[j];
-					for (var k = 0; k < ring.length; k++) {
-						if (!box) {
-							box = { x: ring[k].x, y: ring[k].y, width: 0, height: 0 };
-						} else {
-							var l = Math.min(box.x, ring[k].x);
-							var t = Math.min(box.y, ring[k].y);
-							var r = Math.max(box.x + box.width, ring[k].x);
-							var b = Math.max(box.y + box.height, ring[k].y);
-							box.x = l;
-							box.y = t;
-							box.width = r - l;
-							box.height = b - t;
-						}
-					}
-				}
-			}
-		}
-		var t2 = new Date().getTime();
-		if (window.console && window.console.log) t1 = new Date().getTime();
-		if (window.console && window.console.log) var ctx = canvas.getContext("2d");
-		var sc = Math.min(400 / box.width, 400 / box.height);
-		ctx.fillStyle = "transparent";
-		ctx.fillRect(0, 0, 400, 400);
-		// ctx.lineWidth = 0.5;
-		ctx.strokeStyle = "#ffffff";
-		for (var i = 0; i < records.length; i++) {
-			var record = records[i];
-			if (
-				record.shapeType == ShpType.SHAPE_POLYGON ||
-				record.shapeType == ShpType.SHAPE_POLYLINE
-			) {
-				var shp = record.shape;
-				for (var j = 0; j < shp.rings.length; j++) {
-					var ring = shp.rings[j];
-					if (ring.length < 1) continue;
-					var my_gradient;
-					if (ring.length == 23) {
-						my_gradient = ctx.createRadialGradient(25, 220, 220, 1, 200, 1);
-						my_gradient.addColorStop(0, "#ffb3b3");
-						// my_gradient.addColorStop(0, "green");
-						my_gradient.addColorStop(0.5, "#ff6666");
-						// my_gradient.addColorStop(0.5, "yellow");
-						my_gradient.addColorStop(0.8, "#ff0000");
-						my_gradient.addColorStop(1, "#660000");
-					}
-					if (ring.length == 17) {
-						my_gradient = ctx.createRadialGradient(330, 100, 220, 330, 1, 1);
-						my_gradient.addColorStop(0, "#ffb3b3");
-						// my_gradient.addColorStop(0, "green");
-						my_gradient.addColorStop(0.5, "#ff6666");
-						// my_gradient.addColorStop(0.5, "yellow");
-						my_gradient.addColorStop(0.8, "#ff0000");
-						my_gradient.addColorStop(1, "#660000");
-					}
-					if (ring.length == 21) {
-						my_gradient = ctx.createRadialGradient(330, 350, 220, 320, 350, 1);
-						my_gradient.addColorStop(0, "#ffb3b3");
-						// my_gradient.addColorStop(0, "green");
-						my_gradient.addColorStop(0.5, "#ff6666");
-						// my_gradient.addColorStop(0.5, "yellow");
-						my_gradient.addColorStop(0.8, "#ff0000");
-						my_gradient.addColorStop(1, "#660000");
-					}
-
-					ctx.fillStyle = "#fff";
-					ctx.font = "18px arial";
-					ctx.fillText("Sensor 1", 25, 220);
-
-					ctx.fillStyle = "#fff";
-					ctx.font = "18px arial";
-					ctx.fillText("Sensor 2 ", 250, 30);
-
-					ctx.globalCompositeOperation = "destination-over";
-					ctx.fillStyle = "#fff";
-					ctx.font = "18px arial";
-					ctx.fillText("Sensor 3", 220, 350);
-
-					ctx.fillStyle = my_gradient;
-					// ctx.fillStyle = getFillRecord(data[i], i);
-					ctx.beginPath();
-					ctx.moveTo((ring[0].x - box.x) * sc, 400 - (ring[0].y - box.y) * sc);
-					for (var k = 1; k < ring.length; k++) {
-						ctx.lineTo(
-							(ring[k].x - box.x) * sc,
-							400 - (ring[k].y - box.y) * sc,
-						);
-					}
-					ctx.fill();
-					ctx.stroke();
-				}
-			}
-		}
-		t2 = new Date().getTime();
-	}
-	function getFillRecord(record) {
-		var colors = [
-			"#F1EEF6",
-			"#D4B9DA",
-			"#C994C7",
-			"#DF65B0",
-			"#DD1C77",
-			"#980043",
-		];
-		var popn = parseInt(record.values["POP2005"]);
-		if (!isNaN(popn)) {
-			popn = Math.max(0, Math.min(100000000, popn));
-			var colorIndex = parseInt(((colors.length - 1) * popn) / 100000000);
-			var color = colors[colorIndex];
-			//if (window.console && window.console.log) console.log('popn: ' + popn + ' color: ' + color);
-			return color;
-		}
-		return "rgba(102, 102, 102, .5)";
-	}
-}
-renderMap();
+// HTML ELEMENTS
+const dropdownItemTemperature = document.getElementById("dropdown-item-temperature");
+const dropdownItemPHLevel = document.getElementById("dropdown-item-ph-level");
+const dropdownItemAmmoniaLevel = document.getElementById("dropdown-item-ammonia-level");
+const dropdownItemOxygenLevel = document.getElementById("dropdown-item-oxygen-level");
+const dropdownMenuSelected = document.getElementById("dropdown-menu-selected");
+const barChart = document.getElementById("bar-chart");
+const lineChart = document.getElementById("line-chart");
+// SIDE EFFECT VARIABLES
+let barChartCanvas;
+let lineChartCanvas;
+// SIDE EFFECT FUNCTIONS
+const renderBarChart = (parameter, arrData) => {
+    const labels = ["Sensor 1", "Sensor 2", "Sensor 3"];
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: parameter,
+                data: [arrData[0], arrData[1], arrData[2]],
+                backgroundColor: [
+                    "rgba(136, 200, 247, 0.2)",
+                    "rgba(17, 146, 238, 0.2)",
+                    "rgba(12, 102, 167, 0.2)",
+                ],
+                borderColor: [
+                    "rgb(136, 200, 247)",
+                    "rgb(17, 146, 238)",
+                    "rgb(12, 102, 167)",
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+    const config = {
+        type: "bar",
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    };
+    if (barChartCanvas) {
+        barChartCanvas.destroy();
+        barChart.style.height = "38.1vh";
+        barChart.style.width = "40vw";
+    }
+    // @ts-ignore
+    barChartCanvas = new Chart(barChart, config);
+};
+const renderLineChart = (parameter, arrData) => {
+    const sensor1 = arrData.map((i) => i[0]);
+    const sensor2 = arrData.map((i) => i[1]);
+    const sensor3 = arrData.map((i) => i[2]);
+    const labels = arrData.map((i, index) => `Day ${index + 1}`);
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: `Sensor 1 ${parameter}`,
+                data: sensor1,
+                fill: false,
+                borderColor: "rgb(136, 200, 247)",
+                tension: 0.2,
+            },
+            {
+                label: `Sensor 2 ${parameter}`,
+                data: sensor2,
+                fill: false,
+                borderColor: "rgb(17, 146, 238)",
+                tension: 0.2,
+            },
+            {
+                label: `Sensor 3 ${parameter}`,
+                data: sensor3,
+                fill: false,
+                borderColor: "rgb(12, 102, 167)",
+                tension: 0.2,
+            },
+        ],
+    };
+    const options = {
+        plugins: {
+            autocolors: false,
+            annotation: {
+                annotations: {
+                    line1: {
+                        type: "line",
+                        mode: "vertical",
+                        xMin: 13,
+                        xMax: 13,
+                        borderColor: "red",
+                        borderWidth: 2,
+                        // label: {
+                        // 	content: "TODAY",
+                        // 	enabled: true,
+                        // 	position: "bottom",
+                        // },
+                    },
+                },
+            },
+        },
+    };
+    const config = {
+        type: "line",
+        data: data,
+        options,
+    };
+    if (lineChartCanvas) {
+        lineChartCanvas.destroy();
+        lineChart.style.height = "38.1vh";
+        lineChart.style.width = "40vw";
+    }
+    // @ts-ignore
+    lineChartCanvas = new Chart(lineChart, config);
+};
+const selectParameter = (e) => {
+    const text = e.target.innerText;
+    dropdownMenuSelected.innerText = text;
+    let dummyData;
+    if (text === "Temperature")
+        dummyData = generateDailyDummyData(14, 20, 30);
+    if (text === "pH level")
+        dummyData = generateDailyDummyData(14, 7, 8);
+    if (text === "Ammonia level")
+        dummyData = generateDailyDummyData(14, 7, 12);
+    if (text === "Oxygen level")
+        dummyData = generateDailyDummyData(14, 2, 5);
+    renderBarChart(text, dummyData[0]);
+    renderLineChart(text, dummyData);
+};
+// FRUITFUL FUNCTIONS
+const generateDailyDummyData = (days, min, max) => {
+    const arr = [];
+    for (let i = 0; i < days; i++) {
+        const sensor1 = Math.random() * (max - min + 1) + min;
+        const sensor2 = Math.random() * (max - min + 1) + min;
+        const sensor3 = Math.random() * (max - min + 1) + min;
+        arr.push([
+            parseFloat(sensor1.toFixed(2)),
+            parseFloat(sensor2.toFixed(2)),
+            parseFloat(sensor3.toFixed(2)),
+        ]);
+    }
+    return arr;
+};
+// EVENT LISTENERS
+window.addEventListener("load", (e) => {
+    const dummyData = generateDailyDummyData(14, 20, 30);
+    renderBarChart("Temperature", dummyData[0]);
+    renderLineChart("Temperature", dummyData);
+});
+dropdownItemTemperature.addEventListener("click", (e) => selectParameter(e));
+dropdownItemPHLevel.addEventListener("click", (e) => selectParameter(e));
+dropdownItemAmmoniaLevel.addEventListener("click", (e) => selectParameter(e));
+dropdownItemOxygenLevel.addEventListener("click", (e) => selectParameter(e));
---
+++ index.ts
@@ -0,0 +1,176 @@
+interface ChartType {
+	[key: string]: any;
+}
+// HTML ELEMENTS
+const dropdownItemTemperature = document.getElementById(
+	"dropdown-item-temperature",
+) as HTMLFormElement;
+const dropdownItemPHLevel = document.getElementById(
+	"dropdown-item-ph-level",
+) as HTMLFormElement;
+const dropdownItemAmmoniaLevel = document.getElementById(
+	"dropdown-item-ammonia-level",
+) as HTMLFormElement;
+const dropdownItemOxygenLevel = document.getElementById(
+	"dropdown-item-oxygen-level",
+) as HTMLFormElement;
+const dropdownMenuSelected = document.getElementById(
+	"dropdown-menu-selected",
+) as HTMLFormElement;
+const barChart = document.getElementById("bar-chart") as HTMLFormElement;
+const lineChart = document.getElementById("line-chart") as HTMLFormElement;
+
+// SIDE EFFECT VARIABLES
+let barChartCanvas: ChartType;
+let lineChartCanvas: ChartType;
+
+// SIDE EFFECT FUNCTIONS
+const renderBarChart = (parameter: string, arrData: number[]): void => {
+	const labels = ["Sensor 1", "Sensor 2", "Sensor 3"];
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: parameter,
+				data: [arrData[0], arrData[1], arrData[2]],
+				backgroundColor: [
+					"rgba(136, 200, 247, 0.2)",
+					"rgba(17, 146, 238, 0.2)",
+					"rgba(12, 102, 167, 0.2)",
+				],
+				borderColor: [
+					"rgb(136, 200, 247)",
+					"rgb(17, 146, 238)",
+					"rgb(12, 102, 167)",
+				],
+				borderWidth: 1,
+			},
+		],
+	};
+	const config = {
+		type: "bar",
+		data: data,
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true,
+				},
+			},
+		},
+	};
+	if (barChartCanvas) {
+		barChartCanvas.destroy();
+		barChart.style.height = "38.1vh";
+		barChart.style.width = "40vw";
+	}
+	// @ts-ignore
+	barChartCanvas = new Chart(barChart, config);
+};
+const renderLineChart = (parameter: string, arrData: number[][]): void => {
+	const sensor1 = arrData.map((i) => i[0]);
+	const sensor2 = arrData.map((i) => i[1]);
+	const sensor3 = arrData.map((i) => i[2]);
+	const labels = arrData.map((i, index) => `Day ${index + 1}`);
+	const data = {
+		labels: labels,
+		datasets: [
+			{
+				label: `Sensor 1 ${parameter}`,
+				data: sensor1,
+				fill: false,
+				borderColor: "rgb(136, 200, 247)",
+				tension: 0.2,
+			},
+			{
+				label: `Sensor 2 ${parameter}`,
+				data: sensor2,
+				fill: false,
+				borderColor: "rgb(17, 146, 238)",
+				tension: 0.2,
+			},
+			{
+				label: `Sensor 3 ${parameter}`,
+				data: sensor3,
+				fill: false,
+				borderColor: "rgb(12, 102, 167)",
+				tension: 0.2,
+			},
+		],
+	};
+	const options = {
+		plugins: {
+			autocolors: false,
+			annotation: {
+				annotations: {
+					line1: {
+						type: "line",
+						mode: "vertical",
+						xMin: 13,
+						xMax: 13,
+						borderColor: "red",
+						borderWidth: 2,
+						// label: {
+						// 	content: "TODAY",
+						// 	enabled: true,
+						// 	position: "bottom",
+						// },
+					},
+				},
+			},
+		},
+	};
+	const config = {
+		type: "line",
+		data: data,
+		options,
+	};
+	if (lineChartCanvas) {
+		lineChartCanvas.destroy();
+		lineChart.style.height = "38.1vh";
+		lineChart.style.width = "40vw";
+	}
+	// @ts-ignore
+	lineChartCanvas = new Chart(lineChart, config);
+};
+const selectParameter = (e: any): void => {
+	const text = e.target.innerText;
+	dropdownMenuSelected.innerText = text;
+	let dummyData: number[][];
+	if (text === "Temperature") dummyData = generateDailyDummyData(14, 20, 30);
+	if (text === "pH level") dummyData = generateDailyDummyData(14, 7, 8);
+	if (text === "Ammonia level") dummyData = generateDailyDummyData(14, 7, 12);
+	if (text === "Oxygen level") dummyData = generateDailyDummyData(14, 2, 5);
+	renderBarChart(text, dummyData[0]);
+	renderLineChart(text, dummyData);
+};
+
+// FRUITFUL FUNCTIONS
+const generateDailyDummyData = (
+	days: number,
+	min: number,
+	max: number,
+): number[][] => {
+	const arr = [];
+	for (let i = 0; i < days; i++) {
+		const sensor1 = Math.random() * (max - min + 1) + min;
+		const sensor2 = Math.random() * (max - min + 1) + min;
+		const sensor3 = Math.random() * (max - min + 1) + min;
+		arr.push([
+			parseFloat(sensor1.toFixed(2)),
+			parseFloat(sensor2.toFixed(2)),
+			parseFloat(sensor3.toFixed(2)),
+		]);
+	}
+	return arr;
+};
+
+// EVENT LISTENERS
+window.addEventListener("load", (e) => {
+	const dummyData = generateDailyDummyData(14, 20, 30);
+	renderBarChart("Temperature", dummyData[0]);
+	renderLineChart("Temperature", dummyData);
+});
+dropdownItemTemperature.addEventListener("click", (e) => selectParameter(e));
+dropdownItemPHLevel.addEventListener("click", (e) => selectParameter(e));
+dropdownItemAmmoniaLevel.addEventListener("click", (e) => selectParameter(e));
+dropdownItemOxygenLevel.addEventListener("click", (e) => selectParameter(e));
